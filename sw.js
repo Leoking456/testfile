68eaa36651239d6d70aebe488a66f1cc5a09fc77
Leoking456 <jenujayasingha1@gmail.com>
2026-02-04 22:15:58 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
+self.addEventListner("install",(event)=>{
+  event.waitUntil(
+    caches.open('webapp')then((cache)=>{
+      return caches.addAll([
+        '/',
+        '/index.html'
+        ]);
+    })
+    )
+});
 
+self.addEventListner("fetch",(event)=>{
+  event.respondWith(
+    caches.match(event.request).then((response)=>{
+      return response || fetch(event.request);
+    })
+    );
+});
