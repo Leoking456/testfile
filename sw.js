7e59c61e67f9e037c3217b87906b45553fe0da9a
Leoking456 <jenujayasingha1@gmail.com>
2026-02-08 15:34:02 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,20 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = "pwa-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("webapp").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
