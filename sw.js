53c0be0121183d2a7467e0389b4d0966b15fed0a
Leoking456 <jenujayasingha1@gmail.com>
2026-02-21 15:34:01 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,33 @@
-const CACHE_NAME = "pwa-cache-v1";
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js"
+const CACHE_NAME = 'focus-timer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
+// Install: cache all assets
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then(cache => cache.addAll(['/','index.html'])
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
+// Activate: clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: cache-first strategy
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
